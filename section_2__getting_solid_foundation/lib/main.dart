4ed38fc7e4040a588e4e6337866e20e941a4195a
unknown <philip.ewane97@gmail.com>
2024-03-26 14:02:46 +0100

    Splitting code across files

--- section_2__getting_solid_foundation/lib/main.dart
+++ section_2__getting_solid_foundation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:basics/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,26 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            colors: [Colors.blue, Colors.purple],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello World !",
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
 }
