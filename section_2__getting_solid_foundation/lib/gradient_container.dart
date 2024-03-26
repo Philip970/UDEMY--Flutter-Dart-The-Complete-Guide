8f68bff187cdbf1b7af8eaee2b9cdc8d7ad8df00
unknown <philip.ewane97@gmail.com>
2024-03-26 15:59:26 +0100

    Instance variables

--- section_2__getting_solid_foundation/lib/gradient_container.dart
+++ section_2__getting_solid_foundation/lib/gradient_container.dart
@@ -5,16 +5,16 @@ var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: const [Colors.blue, Colors.purple],
-            begin: startAlignment,
-            end: endAlignment),
+            colors: colors, begin: startAlignment, end: endAlignment),
       ),
       child: const Center(
         child: GrettingText("Bonjour le monde !"),
