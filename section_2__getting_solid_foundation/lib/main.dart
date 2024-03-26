f4234663c7515cb5c2bbc8b634c46330a3de8bdc
unknown <philip.ewane97@gmail.com>
2024-03-26 13:40:58 +0100

    Construction functions

---
+++ section_2__getting_solid_foundation/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.blue, Colors.purple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World !",
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
