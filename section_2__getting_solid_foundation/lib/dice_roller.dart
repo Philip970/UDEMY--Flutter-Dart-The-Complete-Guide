8deae3a8d2df4ce6c692ada2cb26158bd8ddd78f
unknown <philip.ewane97@gmail.com>
2024-03-26 17:07:42 +0100

    Optimization

--- section_2__getting_solid_foundation/lib/dice_roller.dart
+++ section_2__getting_solid_foundation/lib/dice_roller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+final randomizer = Random();
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
@@ -11,12 +13,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = "assets/images/dice-2.png";
+  var currentDiceRoll = 2;
 
   void rollDice() {
     setState(() {
-      var diceRoll = Random().nextInt(6) + 1;
-      activeDiceImage = "assets/images/dice-$diceRoll.png";
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
     print("Changing image...");
   }
@@ -27,7 +28,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          "assets/images/dice-$currentDiceRoll.png",
           width: 200,
         ),
         const SizedBox(
