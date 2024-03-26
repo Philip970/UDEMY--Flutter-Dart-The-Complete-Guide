2ef50a5639d5fb288c499091545a7d109d5f3b77
unknown <philip.ewane97@gmail.com>
2024-03-26 15:37:15 +0100

    Instance variables

--- section_2__getting_solid_foundation/lib/gradient_container.dart
+++ section_2__getting_solid_foundation/lib/gradient_container.dart
@@ -17,7 +17,7 @@ class GradientContainer extends StatelessWidget {
             end: endAlignment),
       ),
       child: const Center(
-        child: GrettingText(),
+        child: GrettingText("Bonjour le monde !"),
       ),
     );
   }
